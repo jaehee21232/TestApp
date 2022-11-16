78b245301b8122a390a5a767b5c38e36691eac5d
jaehee21232 <a62533196@gmail.com>
2022-11-16 20:28:15 +0900

    팔레트 추가

---
+++ lib/page/chatAppMain.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginSinUpScreen extends StatefulWidget {
+  const LoginSinUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginSinUpScreen> createState() => _LoginSinUpScreenState();
+}
+
+class _LoginSinUpScreenState extends State<LoginSinUpScreen> {
+  @override
+  Widget build(BuildContext context) {}
+}
