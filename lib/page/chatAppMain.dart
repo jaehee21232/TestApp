6c343f9ba45c35fda44c9f585e7b7043cbc04e07
jaehee21232 <a62533196@gmail.com>
2022-11-18 13:25:24 +0900

    아래상자 추가

--- lib/page/chatAppMain.dart
+++ lib/page/chatAppMain.dart
@@ -12,6 +12,7 @@ class _LoginSinUpScreenState extends State<LoginSinUpScreen> {
   bool isSingupScreen = true;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Palette.backgroundColor,
       body: Stack(
@@ -32,11 +33,11 @@ class _LoginSinUpScreenState extends State<LoginSinUpScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     RichText(
-                      text: const TextSpan(
+                      text: TextSpan(
                           text: "Welcome",
                           style: TextStyle(
                             letterSpacing: 1.0,
-                            fontSize: 25,
+                            fontSize: size.width * 0.05,
                             color: Colors.white,
                           ),
                           children: [
@@ -44,7 +45,7 @@ class _LoginSinUpScreenState extends State<LoginSinUpScreen> {
                                 text: " to Yummy chat!",
                                 style: TextStyle(
                                     letterSpacing: 1.0,
-                                    fontSize: 25,
+                                    fontSize: size.width * 0.05,
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold))
                           ]),
@@ -59,6 +60,99 @@ class _LoginSinUpScreenState extends State<LoginSinUpScreen> {
                   ],
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            top: 180,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              height: 280,
+              width: size.width - 40,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 15,
+                        spreadRadius: 5)
+                  ]),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSingupScreen = false;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text("LOGIN",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: !isSingupScreen
+                                    ? Palette.activeColor
+                                    : Palette.textColor1,
+                              )),
+                          if (!isSingupScreen)
+                            Container(
+                              margin: EdgeInsets.only(top: 3),
+                              height: 2,
+                              width: 55,
+                              color: Colors.orange,
+                            )
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSingupScreen = true;
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text(
+                            "SIGNUP",
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: !isSingupScreen
+                                    ? Palette.textColor1
+                                    : Palette.activeColor),
+                          ),
+                          if (isSingupScreen)
+                            Container(
+                              margin: EdgeInsets.only(top: 3),
+                              height: 2,
+                              width: 55,
+                              color: Colors.orange,
+                            )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  child: Form(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.account_circle,
+                              color: Palette.iconColor,
+                            ),
+                            enabledBorder: Oupt),
+                      ),
+                    ],
+                  )),
+                )
+              ]),
             ),
           ),
         ],
