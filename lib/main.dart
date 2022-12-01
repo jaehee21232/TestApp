92f05a07e57b58afd9c90e15c2773ed369e0fde5
jaehee21232 <a62533196@gmail.com>
2022-12-01 21:12:43 +0900

    await 추가

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
