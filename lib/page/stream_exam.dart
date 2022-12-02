fe6ab5e3d0d6300b16a15333f48d16f2abecd43a
jaehee21232 <a62533196@gmail.com>
2022-12-02 23:54:58 +0900

    Stream 예제

---
+++ lib/page/stream_exam.dart
@@ -0,0 +1,55 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class StreamExam extends StatelessWidget {
+  const StreamExam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Counter();
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  final int price = 2000;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stream builder"),
+      ),
+      body: StreamBuilder<int>(
+        initialData: price,
+        stream: addStreamValue(),
+        builder: (context, snapshot) /* snapshot은 스트림의 결과물 */ {
+          final pricenumber = snapshot.data.toString();
+          return Center(
+            child: Text(
+              pricenumber,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  color: Colors.blue),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Stream<int> addStreamValue() {
+    return Stream<int>.periodic(
+      Duration(seconds: 1),
+      (computationCount) => price + 1,
+    );
+  }
+}
