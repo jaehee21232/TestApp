a8596da2bb28cd67c2fd9de0ace7de5b5d026a24
jaehee21232 <a62533196@gmail.com>
2022-12-12 17:20:28 +0900

    추가

--- lib/page/chat_screen.dart
+++ lib/page/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -45,7 +46,32 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.logout))
         ],
       ),
-      body: Center(child: Text("Chat screen")),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chats/BSHCDcRNYV93SrCRS02u/message")
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              return Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    docs[index]['text'],
+                    style: TextStyle(fontSize: 20.0),
+                  ));
+            },
+          );
+        },
+      ),
     );
   }
 }
