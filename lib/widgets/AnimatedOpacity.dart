6e0673d788256eabad3c935ac399b43b91034007
jaehee21232 <a62533196@gmail.com>
2022-12-13 18:30:12 +0900

    AnimatedOpacity 위젯 끝

---
+++ lib/widgets/AnimatedOpacity.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedOpacity extends StatefulWidget {
+  const MyAnimatedOpacity({super.key});
+
+  @override
+  State<MyAnimatedOpacity> createState() => _MyAnimatedOpacityState();
+}
+
+class _MyAnimatedOpacityState extends State<MyAnimatedOpacity> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AnimatedOpacity"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
