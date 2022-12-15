84e707c4ab9843fb6924bc0399b1bfb96bf82358
jaehee21232 <a62533196@gmail.com>
2022-12-15 22:45:15 +0900

    위젯, Text 추가

--- lib/page/Fluwidgets.dart
+++ lib/page/Fluwidgets.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/widgets/AnimatedContainer.dart';
+import 'package:testapp/widgets/AnimatedOpacity.dart';
+import 'package:testapp/widgets/Drawer.dart';
+import 'package:testapp/widgets/OrientationBuilder.dart';
+import 'package:testapp/widgets/SnackBar.dart';
+import 'package:testapp/widgets/TanController.dart';
 
 class Fluwidgets extends StatelessWidget {
   const Fluwidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> pages = [];
-    List<String> name = [];
+    const List<Widget> pages = <Widget>[
+      MyAnimatedContainer(),
+      MyAnimatedOpacity(),
+      MyDrawer(),
+      MySnackBar(),
+      MyOrientationBuilder(),
+      MyTabController()
+    ];
+    List<String> name = [
+      "AnimatedContainer",
+      "AnimatedOpacity",
+      "Drawer",
+      "SnackBar",
+      "OrientationBuilder",
+      "TabController",
+    ];
     var size = MediaQuery.of(context).size;
     var width = size.width;
     var height = size.width;
