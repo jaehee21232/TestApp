ae0730ae45b9ec62b94b9125026e82fef4ef805c
jaehee21232 <a62533196@gmail.com>
2022-12-15 17:37:18 +0900

    오리엔테이션빌더 추가

---
+++ lib/widgets/OrientationBuilder.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyOrientationBuilder extends StatefulWidget {
+  const MyOrientationBuilder({super.key});
+
+  @override
+  State<MyOrientationBuilder> createState() => _MyOrientationBuilderState();
+}
+
+class _MyOrientationBuilderState extends State<MyOrientationBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OrientationBuilder"),
+        centerTitle: true,
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.landscape ? 5 : 3,
+            children: List.generate(
+              50,
+              (index) {
+                return Center(child: Text("Item $index"));
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
