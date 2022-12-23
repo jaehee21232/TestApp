6158db3c28a35a02dd02038373e1626a2557a321
jaehee21232 <a62533196@gmail.com>
2022-12-24 00:50:22 +0900

    스타일 변경

--- lib/page/fine_dust.dart
+++ lib/page/fine_dust.dart
@@ -15,23 +15,53 @@ class _FineDustAppState extends State<FineDustApp> {
     return Scaffold(
       body: Center(
           child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("현재 위치 미세먼지 농도"),
           Card(
             child: Column(children: [
-              Row(
-                children: [
-                  Text("얼굴사진"),
-                  Text("80"),
-                  Text("보통"),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text("얼굴사진"),
+                    Text(
+                      "80",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    Text(
+                      "보통",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  Text("얼굴사진"),
-                  Text("80"),
-                  Text("보통"),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [Text("얼굴사진"), Text("11°")],
+                    ),
+                    Text("습도"),
+                    Text("풍속"),
+                  ],
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 50.0)),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    )),
               )
             ]),
           )
