942c811a89f11c0b060f24f0f07683497228d5cf
jaehee21232 <a62533196@gmail.com>
2023-03-15 10:52:30 +0900

    feat: provider 삭제

--- lib/home.dart
+++ lib/home.dart
@@ -2,47 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:testapp/page/Diss_missible.dart';
 import 'package:testapp/page/Form_Validation.dart';
 import 'package:testapp/page/Splash_Screen.dart';
+import 'package:testapp/page/bloc_save.dart';
 import 'package:testapp/page/chat_app_main.dart';
+import 'package:testapp/page/countbloc.dart';
 import 'package:testapp/page/inflearns.dart';
 import 'package:testapp/page/listview_data.dart';
 import 'package:testapp/page/listview_dialog.dart';
 import 'package:testapp/page/onboarding.dart';
-import 'package:testapp/page/provider_test.dart';
-import 'package:testapp/page/stream_exam.dart';
+import 'package:testapp/page/prticles.dart';
 import 'package:testapp/page/flu_widgets.dart';
 import 'package:testapp/page/textanimaion.dart';
 import 'package:testapp/page/web_view.dart';
 
 class HomeBuilder extends StatelessWidget {
-  final List<Widget> pages = const <Widget>[
+  final List<Widget> pages = <Widget>[
     Fluwidgets(),
     OnBoardingPage(),
     ListViewPage(),
     ListViewDataMove(),
-    FisOrder(),
     LoginSinUpScreen(),
-    StreamExam(),
     Inflearns(),
     FormValidation(),
     SwipeToDismiss(),
     SplashScreen(),
     WebViewPage(),
     TextAni(),
+    ParticleWidget(),
+    CounterBloc(),
+    CounterSaveTestPage(),
   ];
   final List<String> texts = const <String>[
     "Widgets",
     "OnBoardingPage",
     "ListView&Dialog",
     "ListView&DataMove",
-    "Provider",
     "ChatApp",
-    "StreamExam",
     "Inflearns",
     "FormValidation",
     "Dissmissible",
     "Loding Page",
     "WebView",
-    "텍스트애니메이션"
+    "텍스트애니메이션",
+    "Particle",
+    "Bloc 카운트 테스트",
+    "Bloc 저장 테스트",
   ];
 
   HomeBuilder({super.key});
